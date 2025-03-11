30c6b05f84a9de7f7601add29d3573c6d6fafafc
Jefferson Farias <jefferson.farias7@gmail.com>
2025-03-11 08:23:43 -0300

    feat: client to request dollar quotation

--- desafio-client-server-api/client/client.go
+++ desafio-client-server-api/client/client.go
@@ -1,6 +1,61 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
 func main() {
-	// continuar a partir daqui
-	// server aparentemente esta pronto
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := http.DefaultClient
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		fmt.Println("timeout from server")
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var respJson map[string]string
+	if err = json.Unmarshal(body, &respJson); err != nil {
+		log.Fatal(err)
+	}
+
+	writeFile(respJson["bid"])
+}
+
+func writeFile(bid string) {
+	file, err := os.OpenFile("cotacao.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data := fmt.Appendf(nil, "Dólar: %s\n", bid)
+
+	if _, err := file.Write(data); err != nil {
+		log.Fatal(err)
+	}
 }
